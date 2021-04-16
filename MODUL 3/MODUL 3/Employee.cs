33bd1b576b44304397ef6fa1ad9ec6a1197a098d
yzdnaufan <ahyaznau@gmail.com>
2021-04-16 14:08:50 +0700

    add login

--- MODUL 3/MODUL 3/Employee.cs
+++ MODUL 3/MODUL 3/Employee.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Chapter2
+namespace MODUL_3
 {
     class Employee
     {
--- MODUL 3/MODUL 3/Form1.cs
+++ MODUL 3/MODUL 3/Form1.cs
@@ -24,7 +24,18 @@ namespace MODUL_3
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            Employee 
+            Employee employee = new Employee();
+            employee.LoginName = tbUsername.Text;
+            employee.Password = tbPassword.Text;
+
+            if(employee.Login (employee.LoginName, employee.Password))
+            {
+                MessageBox.Show("Login Berhasil, ID Anda adalah " + employee.EmployeeID.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Login gagal");
+            }
         }
     }
 }
