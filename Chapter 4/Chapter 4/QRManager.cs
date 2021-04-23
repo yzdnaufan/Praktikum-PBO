edb0483d3571906d3404b2683eb9574a23cd370c
yzdnaufan <ahyaznau@gmail.com>
2021-04-23 14:08:09 +0700

    qr added

---
+++ Chapter 4/Chapter 4/QRManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using QRCoder;
+
+namespace Chapter_4
+{
+    class QRManager
+    {
+
+    }
+}
