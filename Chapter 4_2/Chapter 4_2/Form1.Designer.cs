fd033fce918d3ef27791ba9cf761b61c9454cca8
yzdnaufan <ahyaznau@gmail.com>
2021-04-30 22:03:58 +0700

    Revert "DONE modul 4"
    This reverts commit 8347d6dd3af6a0fda8be53584c0c8c7b4a9e3fb2.

--- Chapter 4_2/Chapter 4_2/Form1.Designer.cs
+++ Chapter 4_2/Chapter 4_2/Form1.Designer.cs
@@ -44,10 +44,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(191, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(143, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(176, 25);
+            this.label1.Size = new System.Drawing.Size(142, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Saldo Akun Umum";
             // 
@@ -55,10 +54,9 @@
             // 
             this.lblSaldo.AutoSize = true;
             this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSaldo.Location = new System.Drawing.Point(263, 87);
-            this.lblSaldo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSaldo.Location = new System.Drawing.Point(197, 71);
             this.lblSaldo.Name = "lblSaldo";
-            this.lblSaldo.Size = new System.Drawing.Size(36, 39);
+            this.lblSaldo.Size = new System.Drawing.Size(29, 31);
             this.lblSaldo.TabIndex = 1;
             this.lblSaldo.Text = "0";
             // 
@@ -66,20 +64,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(116, 181);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(87, 147);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.Size = new System.Drawing.Size(48, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Action:";
             // 
             // rbSetor
             // 
             this.rbSetor.AutoSize = true;
-            this.rbSetor.Location = new System.Drawing.Point(188, 181);
-            this.rbSetor.Margin = new System.Windows.Forms.Padding(4);
+            this.rbSetor.Location = new System.Drawing.Point(141, 147);
             this.rbSetor.Name = "rbSetor";
-            this.rbSetor.Size = new System.Drawing.Size(63, 21);
+            this.rbSetor.Size = new System.Drawing.Size(50, 17);
             this.rbSetor.TabIndex = 3;
             this.rbSetor.TabStop = true;
             this.rbSetor.Text = "Setor";
@@ -88,10 +84,9 @@
             // rbTarik
             // 
             this.rbTarik.AutoSize = true;
-            this.rbTarik.Location = new System.Drawing.Point(296, 181);
-            this.rbTarik.Margin = new System.Windows.Forms.Padding(4);
+            this.rbTarik.Location = new System.Drawing.Point(222, 147);
             this.rbTarik.Name = "rbTarik";
-            this.rbTarik.Size = new System.Drawing.Size(61, 21);
+            this.rbTarik.Size = new System.Drawing.Size(49, 17);
             this.rbTarik.TabIndex = 4;
             this.rbTarik.TabStop = true;
             this.rbTarik.Text = "Tarik";
@@ -101,10 +96,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(60, 220);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(45, 179);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.Size = new System.Drawing.Size(90, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Jumlah Uang:";
             // 
@@ -112,36 +106,31 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(85, 257);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(64, 209);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.Size = new System.Drawing.Size(71, 16);
             this.label4.TabIndex = 6;
             this.label4.Text = "Password:";
             // 
             // tbUang
             // 
-            this.tbUang.Location = new System.Drawing.Point(188, 220);
-            this.tbUang.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUang.Location = new System.Drawing.Point(141, 179);
             this.tbUang.Name = "tbUang";
-            this.tbUang.Size = new System.Drawing.Size(225, 22);
+            this.tbUang.Size = new System.Drawing.Size(170, 20);
             this.tbUang.TabIndex = 7;
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(188, 257);
-            this.tbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPassword.Location = new System.Drawing.Point(141, 209);
             this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(225, 22);
+            this.tbPassword.Size = new System.Drawing.Size(170, 20);
             this.tbPassword.TabIndex = 8;
-            this.tbPassword.UseSystemPasswordChar = true;
             // 
             // btnLanjut
             // 
-            this.btnLanjut.Location = new System.Drawing.Point(188, 300);
-            this.btnLanjut.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLanjut.Location = new System.Drawing.Point(141, 244);
             this.btnLanjut.Name = "btnLanjut";
-            this.btnLanjut.Size = new System.Drawing.Size(227, 27);
+            this.btnLanjut.Size = new System.Drawing.Size(170, 22);
             this.btnLanjut.TabIndex = 9;
             this.btnLanjut.Text = "Lanjut";
             this.btnLanjut.UseVisualStyleBackColor = true;
@@ -149,9 +138,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 385);
+            this.ClientSize = new System.Drawing.Size(437, 313);
             this.Controls.Add(this.btnLanjut);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUang);
@@ -162,7 +151,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblSaldo);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
